318408a7dca4119a15bdd6040cc3f3e4943f20fc
hughes <hughes.bwosi@kenya-airways.com>
2024-01-17 23:59:54 +0300

    setupborad component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { createContext, useState } from "react";
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
 import "./App.css";
+import boardDefault from "./components/Board/Words";
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <Board />
-      <Keyboard />
+      <AppContext.Provider value={(board, setBoard)}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
     </div>
   );
 }
--- src/components/Board/Words.js
+++ src/components/Board/Words.js
@@ -1,4 +1,4 @@
-export const boardDeafult = [
+export const boardDefault = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
   ["", "", "", "", ""],
